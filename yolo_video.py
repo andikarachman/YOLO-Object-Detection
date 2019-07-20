5ad2b606c65acbb6c9730a4fd020270c600188e2
Andika Rachman <rachman.andika@rocketmail.com>
2019-07-20 19:52:57 +0700

    Add files via upload

--- yolo_video.py
+++ yolo_video.py
@@ -6,42 +6,29 @@ import cv2
 import os
 
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--input", required=True,
-	help="path to input video")
-ap.add_argument("-o", "--output", required=True,
-	help="path to output video")
-ap.add_argument("-y", "--yolo", required=True,
-	help="base path to YOLO directory")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-	help="minimum probability to filter weak detections")
-ap.add_argument("-t", "--threshold", type=float, default=0.3,
-	help="threshold when applyong non-maxima suppression")
-args = vars(ap.parse_args())
+def argsParser():
+
+	# construct the argument parse and parse the arguments
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-i", "--input", required=True,
+		help="path to input video")
+	ap.add_argument("-o", "--output", required=True,
+		help="path to output video")
+	ap.add_argument("-y", "--yolo", required=True,
+		help="base path to YOLO directory")
+	ap.add_argument("-c", "--confidence", type=float, default=0.5,
+		help="minimum probability to filter weak detections")
+	ap.add_argument("-t", "--threshold", type=float, default=0.3,
+		help="threshold when applyong non-maxima suppression")
+	args = vars(ap.parse_args())
+
+	return args
 
 
-# load the COCO class labels our YOLO model was trained on
-labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
-LABELS = open(labelsPath).read().strip().split("\n")
-
-# initialize a list of colors to represent each possible class label
-np.random.seed(42)
-COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
-
-# derive the paths to the YOLO weights and model configuration
-WEIGHTS_PATH = os.path.sep.join([args["yolo"], "yolov3.weights"])
-CONFIG_PATH = os.path.sep.join([args["yolo"], "yolov3.cfg"])
-
-# load confidence and threshold level
-CONFIDENCE = args["confidence"]
-THRESHOLD = args["threshold"]
-
-
-def load_run_model(frame, convert_to_rgb=True):
+def load_run_model(frame, config, weights, convert_to_rgb=True):
 
 	# load our YOLO object detector trained on COCO dataset (80 classes)
-	net = cv2.dnn.readNetFromDarknet(CONFIG_PATH, WEIGHTS_PATH)
+	net = cv2.dnn.readNetFromDarknet(config, weights)
 
 	# determine only the *output* layer names that we need from YOLO
 	ln = net.getLayerNames()
@@ -57,7 +44,7 @@ def load_run_model(frame, convert_to_rgb=True):
 	return layerOutputs
 
 
-def generate_bounding_boxes(layerOutputs, W, H):
+def generate_bounding_boxes(layerOutputs, W, H, conf_thresh):
 
 	# initialize our lists of detected bounding boxes, confidences, and
 	# class IDs, respectively
@@ -77,7 +64,7 @@ def generate_bounding_boxes(layerOutputs, W, H):
 
 			# filter out weak predictions by ensuring the detected
 			# probability is greater than the minimum probability
-			if confidence > CONFIDENCE:
+			if confidence > conf_thresh:
 				# scale the bounding box coordinates back relative to the
 				# size of the frame, keeping in mind that YOLO actually
 				# returns the center (x, y)-coordinates of the bounding
@@ -110,7 +97,23 @@ def paint_box_on_object(frame, box, color, label, confidence):
 	cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
 
 
-def run_object_detection():
+def run_object_detection(args):
+
+	# load the COCO class labels our YOLO model was trained on
+	labelsPath = os.path.sep.join([args["yolo"], "coco.names"])
+	LABELS = open(labelsPath).read().strip().split("\n")
+
+	# initialize a list of colors to represent each possible class label
+	np.random.seed(42)
+	COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
+
+	# derive the paths to the YOLO weights and model configuration
+	WEIGHTS_PATH = os.path.sep.join([args["yolo"], "yolov3.weights"])
+	CONFIG_PATH = os.path.sep.join([args["yolo"], "yolov3.cfg"])
+
+	# load confidence and threshold level
+	CONFIDENCE = args["confidence"]
+	THRESHOLD = args["threshold"]
 	
 	# define our input 
 	INPUT = cv2.VideoCapture(args["input"])
@@ -137,11 +140,11 @@ def run_object_detection():
 		H, W = frame.shape[:2]
 
 		# perform a forward pass of the YOLO object detector
-		layerOutputs = load_run_model(frame)
+		layerOutputs = load_run_model(frame, CONFIG_PATH, WEIGHTS_PATH)
 
 		# generate detected bounding boxes, confidences,
 		# and class IDs, respectively
-		boxes, confidences, classIDs = generate_bounding_boxes(layerOutputs, W, H)
+		boxes, confidences, classIDs = generate_bounding_boxes(layerOutputs, W, H, CONFIDENCE)
 
 		# apply non-maxima suppression to suppress weak, overlapping
 		# bounding boxes
@@ -175,5 +178,10 @@ def run_object_detection():
 	INPUT.release()
 
 
+def main():
+    args = argsParser()
+    run_object_detection(args)
+
 # Run object detection
-run_object_detection()
+if __name__ == '__main__':
+    main()
